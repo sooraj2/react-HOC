f7fa8db9a0088bef7211b5e53c8faf60a5738768
sooraj2 <soorajkumar.cs17@iba-suk.edu.pk>
2021-03-31 19:42:31 +0500

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
-import React from "react";
+import React,{useState, useEffect} from "react";
 import "./style.css";
+
+
 //import Component from "./Component";
 
 export default function App() {
+  
   const Now = x(Component);
+  const [names, setNames]= useState([]);
+
+useEffect(()=>{
+fetch('https://randomuser.me/api?results=10').
+  then((res)=> res.json()).
+  then((json)=> json.results).
+  then((data)=> data.map((user)=>user.name.first)).
+  then((aakhri)=> setNames(aakhri))
+},[]);
+  
+
 
   return (
     <div>
       <Now />
+      <ul>
+        {names.map((name)=>(<li>
+        {name}
+        </li>))}
+      </ul>
+
     </div>
   );
 }
